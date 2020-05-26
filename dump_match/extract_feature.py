1414e2e15d3463f34003e861fd1c7f6a2da4c163
pultarmi <milan.pultar@gmail.com>
2020-05-26 22:46:42 +0200

    fast push

--- dump_match/extract_feature.py
+++ dump_match/extract_feature.py
@@ -37,7 +37,7 @@ class ExtractSIFT(object):
         kp = np.array([[_kp.pt[0], _kp.pt[1], _kp.size, _kp.angle] for _kp in cv_kp])  # N*4
         # print(img.shape)
 
-        img = PIL.Image.fromarray(img, 'RGB').convert('L')
+        img = Image.fromarray(img, 'RGB').convert('L')
         for k in kp:
             D2pt = k
             left, top, right, bottom = D2pt[0] - 32, D2pt[1] - 32, D2pt[0] + 32, D2pt[1] + 32
